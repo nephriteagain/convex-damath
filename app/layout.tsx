95b7cb9c179ed2f6390838acd5f2fc663312589e
nephriteagain <oneabove232@gmail.com>
2023-09-15 18:33:27 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,10 @@ import { Provider } from 'react-redux'
 import { store } from '@/redux/store'
 import ConvexClientProvider from '@/components/convexProvider/ConvexClientComponent'
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Damath Online',
-  description: 'An educational board game based on checkers',
-}
 
 export default function RootLayout({
   children,
